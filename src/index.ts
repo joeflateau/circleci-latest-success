93ccda7ce44539689e5171fcbc582be95abd4a07
Joe Flateau <joe@joeflateau.net>
2022-03-11 15:52:15 -0500

    need to parse pipeline reference from build url

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Axios from 'axios';
 
-async function printLastSuccessRevision(pipeline: string, branch: string) {
+async function printLastSuccessRevision(buildUrl: string, branch: string) {
   try {
+    const pipeline = getPipelineFromBuildUrl(buildUrl);
+    console.error({ buildUrl, branch, pipeline });
     const success = await findSuccessRevision(pipeline, branch);
     console.log(success);
   } catch (err) {
@@ -12,22 +14,25 @@ async function printLastSuccessRevision(pipeline: string, branch: string) {
 async function findSuccessRevision(pipeline: string, branch: string) {
   let nextPageToken: string | null = null;
   do {
-    const { data: response } = await Axios.get<{
-      next_page_token: string | null;
-      items: {
-        id: string;
-        vcs: { revision: string };
-      }[];
-    }>(
-      `https://circleci.com/api/v2/project/${pipeline}/pipeline?branch=${branch}`,
-      {
-        headers: { 'Circle-Token': process.env.CIRCLE_API_TOKEN ?? '' },
-      }
-    );
+    const fetchUrl = `https://circleci.com/api/v2/project/${pipeline}/pipeline?branch=${branch}`;
+    const response: {
+      data: {
+        next_page_token: string | null;
+        items: {
+          id: string;
+          vcs: { revision: string };
+        }[];
+      };
+    } = await Axios.get(fetchUrl, {
+      params: { next_page_token: nextPageToken || undefined },
+      headers: { 'Circle-Token': process.env.CIRCLE_API_TOKEN ?? '' },
+    });
 
-    nextPageToken = response.next_page_token;
+    const data = response.data;
 
-    for (const item of response.items) {
+    nextPageToken = data.next_page_token;
+
+    for (const item of data.items) {
       if (await wasSuccessful(item.id)) {
         return item.vcs.revision;
       }
@@ -36,13 +41,16 @@ async function findSuccessRevision(pipeline: string, branch: string) {
 }
 
 async function wasSuccessful(pipelineId: string) {
-  const workflow = await Axios.get<{ items: { status: string }[] }>(
-    `https://circleci.com/api/v2/pipeline/${pipelineId}/workflow`,
-    {
-      headers: { 'Circle-Token': process.env.CIRCLE_API_TOKEN ?? '' },
-    }
-  );
+  const fetchUrl = `https://circleci.com/api/v2/pipeline/${pipelineId}/workflow`;
+  const workflow = await Axios.get<{ items: { status: string }[] }>(fetchUrl, {
+    headers: { 'Circle-Token': process.env.CIRCLE_API_TOKEN ?? '' },
+  });
   return workflow.data.items.every((wf) => wf.status === 'success');
+}
+
+function getPipelineFromBuildUrl(buildUrl: string) {
+  // https://circleci.com/[platform]]/[username]/[repo]/tree/master
+  return buildUrl.split('circleci.com/')[1].split('/').slice(0, 3).join('/');
 }
 
 printLastSuccessRevision(process.argv[2], process.argv[3]);
