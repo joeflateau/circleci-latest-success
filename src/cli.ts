26362a37f1e8b2a07291cf03edeacf680a2d728c
Joe Flateau <joe@joeflateau.net>
2022-03-11 22:26:34 -0500

    let commander parse cli args

--- src/cli.ts
+++ src/cli.ts
@@ -1,20 +1,35 @@
 #!/usr/bin/env node
 
+import { Command } from 'commander';
 import { findSuccessRevision, getPipelineFromBuildUrl } from '.';
 
-async function printLastSuccessRevision(buildUrl: string, branch: string) {
-  try {
-    const pipeline = getPipelineFromBuildUrl(buildUrl);
-    console.error({ buildUrl, branch, pipeline });
-    const success = await findSuccessRevision(
-      pipeline,
-      branch,
-      process.env.CIRCLE_API_TOKEN ?? ''
-    );
-    console.log(success);
-  } catch (err) {
-    console.error(err);
-  }
+async function main() {
+  await new Command()
+    .option('-v, --verbose', 'verbose output')
+    .argument('<buildUrl>')
+    .argument('<branch>')
+    .action(async (buildUrl, branch, { verbose }) => {
+      const pipeline = getPipelineFromBuildUrl(buildUrl);
+
+      if (verbose) {
+        Object.entries({ buildUrl, branch, pipeline }).forEach(([key, value]) =>
+          console.error(`${key}: ${value}`)
+        );
+      }
+
+      try {
+        const success = await findSuccessRevision(
+          pipeline,
+          branch,
+          process.env.CIRCLE_API_TOKEN ?? ''
+        );
+
+        process.stdout.write(success);
+      } catch (err: any) {
+        console.error(err.message);
+      }
+    })
+    .parseAsync(process.argv);
 }
 
-printLastSuccessRevision(process.argv[2], process.argv[3]);
+main();
